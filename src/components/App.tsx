425be063896584c62de78dddcd98fc053a85f653
yangTauTauAjah <tesakun6464@gmail.com>
2023-03-20 18:19:51 +0700

    creating drawer

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Drawer from './drawer'
+// import './App.css'
+
+function App() {
+  
+
+  return (
+    <Drawer />
+  )
+}
+
+export default App
