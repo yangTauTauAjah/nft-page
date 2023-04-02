9e5d60ed9c9c35598a4095740df0e3ae0dde1718
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-02 16:28:44 +0700

    app.tsx wrapper

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Drawer from './drawer'
+import Drawer from './drawer_1'
 // import './App.css'
 
 function App() {
