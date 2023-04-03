56bfe74603a9da55e179ae7fc911f6f3167ff725
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-03 10:13:01 +0700

    putting all components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Header from "./Header";
+/* import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg"; */
 import { Box, Container, Stack, styled } from "@mui/system";
 import { createTheme, ThemeProvider } from "@mui/material";
 import { themeSettings } from "@/theme";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import Recommendation from "./Recommendation";
 // import "@/styles/App.css";
 
 const Theme = createTheme(themeSettings);
@@ -17,27 +19,33 @@ function App() {
 
   return (
     <ThemeProvider theme={Theme}>
-      {/* <Drawer /> */}
       <Header />
-      <Box
-        component={"div"}
-        sx={(theme) => {
-          console.log(theme.breakpoints.down('sm'))
-          return {
-            color: "black",
-            background: theme.palette.primary[500],
-            [theme.breakpoints.down('sm')]: {
-              background: theme.palette.primary[300]
-            }
-          }
-        }}
-        // onClick={() => setState({})}
-      >
-        Hello World
-      </Box>
-      {/* <Drawer /> */}
+      <HeroSection />
+      <Recommendation />
     </ThemeProvider>
   );
 }
 
 export default App;
+
+
+
+
+
+{/* <Box
+  component={"div"}
+  sx={(theme) => {
+    console.log(theme.breakpoints.down('sm'))
+    return {
+      color: "black",
+      background: theme.palette.primary[500],
+      [theme.breakpoints.down('sm')]: {
+        background: theme.palette.primary[300]
+      }
+    }
+  }}
+  onClick={() => setState({})}
+>
+  Hello World
+</Box>
+<Drawer /> */}
