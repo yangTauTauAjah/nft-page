957ff7c5ddc1aa1bc1d7107a6c75183d033dd7bf
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-03 08:29:22 +0700

    create header

---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Box, Container, Stack, styled } from "@mui/system";
+import { createTheme, ThemeProvider, Button, Link } from "@mui/material";
+import { themeSettings } from "@/theme";
+
+const FlexSpaceBetweenCentered = styled("div")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+}));
+
+const Main = styled(FlexSpaceBetweenCentered)(({ theme }) => ({
+  height: theme.spacing(10),
+  paddingInline: theme.spacing(6.2)
+}));
+
+const ButtonStyle = {
+  height: "60px",
+  width: "164px",
+  borderRadius: "25px",
+  fontSize: "1rem"
+}
+
+const LinkList = styled(FlexSpaceBetweenCentered)(({ theme }) => ({
+  justifyContent: "initial",
+  gap: '2.5rem'
+}));
+
+function Header() {
+  return (
+    <Main>
+      Test
+      <LinkList>
+        <Link sx={{color: 'gray', fontStyle: 'none', cursor: 'default'}}>Auction</Link>
+        <Link sx={{color: 'gray', fontStyle: 'none', cursor: 'default'}}>Community</Link>
+        <Link sx={{color: 'gray', fontStyle: 'none', cursor: 'default'}}>Explore</Link>
+      </LinkList>
+      <Button
+        sx={ButtonStyle}
+        variant="contained"
+      >
+        My Wallet
+      </Button>
+    </Main>
+  );
+}
+
+export default Header;
