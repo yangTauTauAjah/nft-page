f4a8910524964b471cfa973bbab1c62e355a7bed
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-03 07:29:49 +0700

    configuration

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Drawer from './drawer_1'
-// import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Drawer from "./Drawer";
+import { Box, Container, Stack, styled } from "@mui/system";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { themeSettings } from "@/theme";
+import "@/styles/App.css";
+
+const Theme = createTheme(themeSettings);
+
+styled;
 
 function App() {
-  
-
   return (
-    <Drawer />
-  )
+    <ThemeProvider theme={Theme}>
+      {/* <Drawer /> */}
+      <Box component={"div"} sx={{ background: (theme) => theme.palette.primary[500], color: "black" }}>
+        Hello World
+      </Box>
+      {/* <Drawer /> */}
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
