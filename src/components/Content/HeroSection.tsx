1b198b3fa862ce22774ff86075dc84c3e0083b2c
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-05 08:57:05 +0700

    inserting image

--- src/components/Content/HeroSection.tsx
+++ src/components/Content/HeroSection.tsx
@@ -1,14 +1,17 @@
 import { styled } from "@mui/system";
 import { Button } from "@mui/material";
+import img2 from '@/assets/images/img2.png'
+import { useTheme, Box } from "@mui/material";
 
 const Main = styled("div")({
   display: "flex",
-  flexDirection: 'column',
+  flexDirection: 'column-reverse',
   justifyContent: 'space-between',
   alignItems: 'center',
   width: "100%",
   aspectRatio: "12/6",
-  background: "rgba(0,0,0,.4)"
+  borderRadius: '0 0 20px 20px',
+  overflow: 'hidden',
 });
 
 const Hero = styled("div")({
@@ -18,7 +21,7 @@ const Hero = styled("div")({
   marginBottom: '4rem',
   flexDirection: "column",
   gap: '2.5rem',
-  alignItems: "center"
+  alignItems: "center",
 });
 
 const ExtendedButton = styled(Button)({
@@ -29,12 +32,16 @@ const ExtendedButton = styled(Button)({
   fontSize: '0.9rem',
   background: [
     `rgb(255,255,255);`,
-    `linear-gradient(163deg, rgba(240,240,240,0.25) 0%, rgba(255,255,255,0) 100%);`
+    `linear-gradient(163deg, rgba(240,240,240,0.5) 0%, rgba(255,255,255,0.1) 100%);`
   ],
+  border: 'solid rgba(255,255,255,0.5) 1.5px',
   color: 'rgba(255,255,255, 0.5)'
 });
 
 function HeroSection() {
+
+  const Theme = useTheme()
+
   return (
     <Main>
       <div
@@ -42,12 +49,21 @@ function HeroSection() {
           position: "absolute",
           top: 0,
           width: "100%",
-          height: "100%"
+          height: "100%",
         }}
-      ></div>
-      <div></div>
+      >
+        <div style={{ position: 'absolute', width: '100%', height: '100%', background: 'rgba(3, 10, 35, 0.8)', top: 0 }} />
+        <img style={{ zIndex: -1, width: '100%', top: '-50%' }} src={img2} />
+      </div>
       <Hero>
-        <h1 style={{ textAlign: "center", fontSize: 100 }}>Collect Your NFTs Here!</h1>
+        <Box component='h1' sx={{
+          /* @ts-ignore */
+          background: `linear-gradient(to bottom, ${Theme.palette.primary[800]}, ${Theme.palette.primary[500]})`,
+          WebkitTextFillColor: 'transparent',
+          WebkitBackgroundClip: 'text',
+          textAlign: "center",
+          fontSize: 100,
+        }}>Collect Your NFTs Here!</Box >
         <ExtendedButton>Find your NFT</ExtendedButton>
       </Hero>
     </Main>
