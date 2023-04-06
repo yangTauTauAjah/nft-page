4b1d33985789bc427a89daa0507a6e15572970aa
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-06 15:42:28 +0700

    Edit the content and add loading and success modal screen

--- src/components/Footer/Upper.tsx
+++ src/components/Footer/Upper.tsx
@@ -1,6 +1,7 @@
-import { styled } from "@mui/system";
-import { Button, Link, Stack, Input } from "@mui/material";
+import React from 'react'
+import { Button, Link, Stack, styled, Input, IconButton, Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText, Backdrop, CircularProgress } from "@mui/material";
 import { useTheme } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close';
 import Logo from "../Logo";
 
 const Grid = styled("div")({
@@ -20,28 +21,39 @@ const ExtendedLink = styled(Link)({
   cursor: 'pointer'
 })
 
+const isValidEmail = (val: string) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val)
+
 function Upper() {
 
   const Theme = useTheme()
+  const [open, setOpen] = React.useState(false);
+  const [error, setError] = React.useState(false)
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+
+    if (!loading) return
+    setTimeout(() => {
+       setOpen(true)
+    }, 1000)
+
+  }, [loading])
+
+  React.useEffect(() => {
+    if (open) setLoading(false)
+  }, [open])
+  
 
   return (
     // @ts-ignore
     <Grid style={{ background: `${Theme.palette.primary[700]}20`, color: 'white', fontWeight: 600 }}>
       <Stack gap={5}>
-        {/* <div
-          style={{
-            width: "255px",
-            height: "50px",
-            background: "rgba(0,0,0,.2)"
-          }}
-        /> */}
-        <Stack direction='row' sx={{alignItems: 'center'}}>
-          <Logo fill='white'/>
-          <h3>MahyuNFT</h3>
+        <Stack direction='row' sx={{ alignItems: 'center', gap: '1rem' }}>
+          <Logo fill='white' />
+          <h3>NFTopia</h3>
         </Stack>
-        <p style={{ fontSize: "1.125rem" }}>
-          MahyuNFT is the largest NFTs Marketplace of Etherium (ETH). Buy,
-          sell, and discover exclusive digital items.
+        <p style={{ fontSize: "0.8rem", fontWeight: 300 }}>
+          NFTopia is more than an NFT marketplace - it's a community of passionate individuals who share a love for digital art and blockchain technology.
         </p>
       </Stack>
       <Stack sx={{ justifyContent: "space-between" }}>
@@ -51,7 +63,7 @@ function Upper() {
         <ExtendedLink>How to Buy and Sell</ExtendedLink>
       </Stack>
       <Stack gap={3}>
-        <p>Subscribe MahyuNFT for newsletter right now!</p>
+        <p>Subscribe NFTopia for newsletter right now!</p>
         <Stack gap={1} direction="row">
           <div
             style={{
@@ -64,12 +76,24 @@ function Upper() {
             }}
           >
             <Input
+              id='email-input'
               type="email"
               placeholder="Insert your email here"
+              error={error}
+              onChange={() => {
+                // @ts-ignore
+                if (isValidEmail(document.querySelector('#email-input')?.value)) setError(false)
+                else setError(true)
+              }}
               sx={{ fontSize: "1.125rem", width: "100%", height: "2rem" }}
             />
           </div>
           <Button
+            onClick={() => {
+              // @ts-ignore
+              if (document.querySelector('#email-input')?.value === '') return
+              if (!error) setLoading(true)
+            }}
             variant="contained"
             sx={{
               width: "6rem",
@@ -78,8 +102,30 @@ function Upper() {
               height: "3.75rem"
             }}
           >
-            Sent
+            Send
           </Button>
+          <Dialog
+            open={open}
+            onClose={() => setOpen(false)}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title" sx={{ fontSize: '1.5rem' }}>Success</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Your Email has successfully been sent
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => setOpen(false)}>OK</Button>
+            </DialogActions>
+          </Dialog>
+          <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={loading}
+          >
+            <CircularProgress color="inherit" />
+          </Backdrop>
         </Stack>
       </Stack>
     </Grid>
